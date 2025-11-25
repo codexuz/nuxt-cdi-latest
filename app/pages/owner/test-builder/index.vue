9916d961a8e1807419a10753f878cc9a8bad9694
codexuz <examonlineedumo@gmail.com>
2025-11-25 11:21:49 +0500

    "main"

--- app/pages/owner/test-builder/index.vue
+++ app/pages/owner/test-builder/index.vue
@@ -533,7 +533,7 @@ const deleteTest = async (testId) => {
     }
 
     await $fetch(
-      `${baseURL}/ielts/centers/${activeCenter.value.id}/tests/${testId}/hard`,
+      `${baseURL}/ielts/centers/${activeCenter.value.id}/tests/${testId}`,
       {
         method: "DELETE",
         headers: {
